7438c75deca533a2161be4d81c09df186cb93a0d
scarreno-vrgroup_bicevida <scarreno.vrgroup@bicevida.cl>
2026-02-17 02:01:54 -0300

    fix

--- producer-queue-1/src/main/java/com/transp_national/producer_queue_1/domain/model/Location.java
+++ producer-queue-1/src/main/java/com/transp_national/producer_queue_1/domain/model/Location.java
@@ -1,10 +1,11 @@
 package com.transp_national.producer_queue_1.domain.model;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public record Location(
+public record Location (
         String vehicleId,
         Double latitude,
         Double longitude,
         LocalDateTime timestamp
-) {}
+) implements  Serializable{}
